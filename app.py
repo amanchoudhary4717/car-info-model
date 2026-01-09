a75ddb92e8340eb093c14b6873f4eab454191068
Aman Choudhary <abamanchoudhary4717@gmail.com>
2026-01-10 01:55:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-MODEL_PATH = "./car-gpt2-lora"
+import os
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
+os.environ["HF_HUB_DISABLE_TELEMETRY"] = "1"
+
+
+MODEL_PATH = "car-gpt2-lora"  # no leading "./"
 
 @st.cache_resource
 def load_model():
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-    model = AutoModelForCausalLM.from_pretrained(MODEL_PATH)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, local_files_only=True)
+    model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, local_files_only=True)
     return pipeline("text-generation", model=model, tokenizer=tokenizer)
+
 
 pipe = load_model()
 
@@ -26,3 +32,4 @@ if st.button("Generate"):
                       temperature=temperature, top_p=0.92,
                       repetition_penalty=repetition_penalty)[0]["generated_text"]
     st.write(output)
+
